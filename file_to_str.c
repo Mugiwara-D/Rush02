802695df0239800de2119679d1637f85186c1a61
Matiya Deruelle <maderuel@e2r4p23.clusters.42paris.fr>
2022-10-01 15:05:23 +0200

    file to str mets le contenue du fichier dans une str

---
+++ file_to_str.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open_parse.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maderuel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 12:01:13 by maderuel          #+#    #+#             */
+/*   Updated: 2022/10/01 15:03:23 by maderuel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+char	*ft_open(char *file)
+{
+	char *nbf;
+	int	fd;
+	int b;
+	int	c;
+	
+	nbf = (char *) malloc(700 * sizeof(char));
+	fd = open(file, O_RDONLY );
+	if (fd < 0)
+	{
+		printf("ERROR");
+		return (NULL);
+	}
+	b = read(fd,nbf,700);
+	if (b < 0)
+	{
+		printf("error");
+		return (NULL);
+	}
+	nbf[b] = '\0';
+	c = close(fd);
+	if (c < 0)
+	{
+		printf("erno");
+		return (NULL);
+	}
+	return(nbf);
+}
